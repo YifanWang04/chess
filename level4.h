edbc9452e7739059746d18e412e95e7500c34904
Yifan_Wang <ivan.yifan.wang@outlook.com>
2024-07-26 10:42:33 -0400

    update computer level4 6

--- level4.h
+++ level4.h
@@ -4,6 +4,7 @@
 #include "computer.h"
 #include <vector>
 #include <tuple>
+#include <deque>
 
 class Level4 : public Computer {
 public:
@@ -15,6 +16,10 @@ private:
     std::vector<std::tuple<int, int, int, int>> getAllPossibleMoves(Board* board, int color);
     int evaluateMove(Board* board, int fromRow, int fromCol, int toRow, int toCol);
     int getPieceValue(Piece* piece);
+    bool isRepeatedCheck(int fromRow, int fromCol, int toRow, int toCol);
+
+    std::deque<std::tuple<int, int, int, int>> recentChecks; // To track recent check moves
+    static const int maxRecentChecks = 10; // Maximum number of recent check moves to track
 };
 
 #endif // LEVEL4_H
